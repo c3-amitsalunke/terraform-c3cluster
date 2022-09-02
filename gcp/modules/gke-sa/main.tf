10db2b7134604bee8f140fe578d09838523aa1b7
Amit Salunke <amit.salunke@c3.ai>
2022-09-02 12:25:13 -0700

    Reset

--- gcp/modules/gke-sa/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_service_account_iam_binding" "kubernetes_service_account_iam_binding" {
-  service_account_id = var.service_account_id
-  role               = "roles/iam.workloadIdentityUser"
-  members            = var.members
-}
